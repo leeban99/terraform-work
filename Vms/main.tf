51e2070ee69df4a68713e3b3a4704a4b5cb56149
leeban99 <leeban99@gmail.com>
2021-08-24 12:46:07 +0530

    updated main file

--- Vms/main.tf
+++ Vms/main.tf
@@ -5,13 +5,6 @@ provider "google" {
  zone        = "us-central1-a"
 }
 
-resource "google_compute_instance_group" "test" {
-  name        = "terraform-test"
-  description = "Terraform test instance group"
-  zone        = "us-central1-a"
-  network     = google_compute_network.vpc_network.id
-}
-
 resource "google_compute_network" "vpc_network" {
  name = "vpc-terraform-network"
  auto_create_subnetworks = false
