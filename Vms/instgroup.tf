704b58f445e1fa27ff3faf09e205d35b6192ea1f
leeban99 <leeban99@gmail.com>
2021-08-24 13:50:51 +0530

    updated main file

--- Vms/instgroup.tf
+++ Vms/instgroup.tf
@@ -2,5 +2,6 @@ resource "google_compute_instance_group" "test" {
   name        = "terraform-test"
   description = "Terraform test instance group"
   zone        = "us-central1-a"
-  network     = google_compute_network.vpc_network.id
+  #network     = google_compute_network.vpc_network.id
+  nstances = [google_compute_instance.vm_instance.id]
 }
