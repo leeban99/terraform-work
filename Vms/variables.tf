cccbf795f21cbf6ea7051f678c87d98f68803768
leeban99 <leeban99@gmail.com>
2021-09-16 20:07:18 +0530

    adding file

---
+++ Vms/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "turnkey-cooler-316513"
+ }
+
+variable "credentials_file" { 
+    default = "credentials.json"
+}
+
+variable "region" {
+  default = "us-east1"
+}
+
+variable "zone" {
+  default = "us-east1-c"
+}
